3fd73c1835f2a40725a888885ea4eacb6bbc3b04
Vicliu1213 <166167276+Vicliu1213@users.noreply.github.com>
2026-01-24 09:31:57 +0800

    main

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension charliermarsh.ruff
+code --install-extension eamodio.gitlens
+code --install-extension GitHub.copilot
+code --install-extension ms-vscode.makefile-tools
+code --install-extension ms-python.debugpy
+code --install-extension mhutchie.git-graph
+code --install-extension tamasfe.even-better-toml
+code --install-extension redhat.vscode-yaml
+code --install-extension gruntfuggly.todo-tree
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension chrisdias.vscode-opennewinstance
